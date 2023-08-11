f72d228644c7157a76c013d8e1bea0d9450b9db9
swatigali <swatigali99@gmail.com>
2023-08-11 18:30:26 +0530

    mypod

---
+++ k8s_notes/Kubernetes_Master_installation.sh
@@ -0,0 +1,15 @@
+sudo apt-get install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubeadm token create --print-join-command
---
+++ k8s_notes/Kubernetes_Node_installation.sh
@@ -0,0 +1,9 @@
+sudo apt-get install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
